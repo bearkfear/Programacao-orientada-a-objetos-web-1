02a8648d3130d8164ec30186cd59505db8ce4ae3
Enrico <33099123+bearkfear@users.noreply.github.com>
2018-09-03 11:20:52 -0300

    JavaDoc
    Adição da documentação java para a classe de conexão com banco de dados

--- src/java/br/csi/dao/ConectaDB_Postgress.java
+++ src/java/br/csi/dao/ConectaDB_Postgress.java
@@ -1,51 +1,41 @@
 package br.csi.dao;
 
-
-/**
- * importação de java.Connection referencia a classe Connection
- * Será importante para retornar um tipo de conexão 
-*/
 import java.sql.Connection;
-
-/**
- * Importação da classe DriverManager referente 
- 
- */
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ *
+ * Classe para conexão com banco de dados PostgreSQL
+ *
+ * @author BearkFear
+ */
 public class ConectaDB_Postgress {
-    
-    /**
-     * Classe para conexão com banco de dados postgres
-     * 
-     * 
-     * Declaração das constantes estáticas para o driver, 
-     * url, 
-     * usuário do banco de dados e 
-     * a senha
-    */
-    
+
     // DRIVER
     private static final String DRIVER = "org.postgresql.Driver";
-    
+
     // URL
     private static final String URL = "jdbc:postgresql://localhost:5432/rotas2018";
-    
+
     // USUARIO
     private static final String USER = "admin";
-    
+
     // SENHA DO USUARIO
     private static final String PASSWORD = "0000";
-    
-    
-    
+
+    /**
+     *
+     * Metodo que retorna conexão com banco de dados Retorna uma Connection
+     *
+     * @return
+     */
     public Connection getConexao() {
         Connection conn = null;
 
         try {
             Class.forName(DRIVER); // escolhe o banco apartir do driver
-            conn = DriverManager.getConnection(URL , USER, PASSWORD); // conecta no banco e recebe uma conexão
+            conn = DriverManager.getConnection(URL, USER, PASSWORD); // conecta no banco e recebe uma conexão
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         } catch (SQLException e) {
