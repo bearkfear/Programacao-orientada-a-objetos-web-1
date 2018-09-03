e0c04a4f7370051b7b660bcb94114aebdfcc49c0
Enrico <33099123+bearkfear@users.noreply.github.com>
2018-09-03 11:20:05 -0300

    Correção de  bug
    Correção de bugs na conexão com banco de dados e inclusão de javaDocs para auxiliar a programação

--- src/java/br/csi/dao/FuncionarioDAO.java
+++ src/java/br/csi/dao/FuncionarioDAO.java
@@ -4,46 +4,70 @@
  * and open the template in the editor.
  */
 package br.csi.dao;
+
 import br.csi.model.Funcionario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 /**
- * 
+ *
+ * Classe FuncionarioDAO que conecta com banco de dados como CRUD
+ *
+ *
  * @author Enrico
  */
+
+/*
+
+Tabela no banco de dados 
+
+CREATE TABLE funcionario(
+    id serial, 
+    id_usuario int,
+    salario float,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    PRIMARY KEY (ID)
+)
+ */
 public class FuncionarioDAO {
-    
-    
-    
-    /*
-     * Não está funcionando, está retornando nullPointerException
-     * 
-     * 
-     * Tem que fazer correções 
-     * 
-     * 
-     * 
-     */
+
     public static void main(String[] args) {
         Funcionario f = new FuncionarioDAO().read(2);
         System.out.println("Salario: " + f.getSalario());
     }
+
     
     
-    public boolean create (Funcionario funcionario) {
+    /**
+     * Metodo responsável por armazenar e criar uma ocorrencia de entidade no banco de dados
+     * 
+     * 
+     * @param funcionario
+     * @return 
+     */
+    public boolean create(Funcionario funcionario) {
         return false;
     }
-    
-    public Funcionario read (int id) {
+
+    /**
+     * Metodo leitura de um funcionario, le as informações do funcionario no
+     * banco de dados e retorna o funcionario
+     *
+     * Utiliza o codigo de identificação do usuario para retornar as informações
+     *
+     * @param id
+     * @return 
+     */
+    public Funcionario read(int id) {
         try (Connection conn = new ConectaDB_Postgress().getConexao()) {
-            String SQL = "SELECT * FROM funcionario WHERE funcionario.id_usuario = usuario.id AND funcionario.id = ?";
+            String SQL = "SELECT * FROM funcionario, usuario WHERE funcionario.id_usuario = usuario.id AND funcionario.id = ?";
             PreparedStatement pre = conn.prepareStatement(SQL);
             pre.setInt(1, id);
             ResultSet rs = pre.executeQuery();
             while (rs.next()) {
-                
+
                 Funcionario funcionario = new Funcionario();
                 funcionario.setEmail(rs.getString("email"));
                 funcionario.setSenha(rs.getString("senha"));
@@ -51,11 +75,11 @@ public class FuncionarioDAO {
                 funcionario.setSalario(rs.getFloat("salario"));
                 return funcionario;
             }
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        
+
         return null;
     }
 }
